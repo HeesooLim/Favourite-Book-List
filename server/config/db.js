1f7db0a9f00a19ab42cfe92bacc8d11854b54ea4
heesoolim <hlim29@my.centennialcollege.ca>
2020-10-26 19:18:35 -0400

    change URI to point MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://heesoo:Fc84t9GyfJ8SIpt4@books.e5ilh.mongodb.net/books229?retryWrites=true&w=majority"
 };
